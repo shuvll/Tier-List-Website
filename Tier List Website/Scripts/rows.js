394b6e13b6a8c89512450b64f8e7102096cc014d
shuvll <ethanhogge952@gmail.com>
2022-03-20 17:44:02 -0600

    FINAL

---
+++ Tier List Website/Scripts/rows.js
@@ -0,0 +1,21 @@
+const rows = document.querySelectorAll('.row');
+const colors = ['#ff7f7f', '#ffbf7f', '#ffdf7f', '#ffff7f', '#bfff7f', '#7fff7f'];
+
+const onDragOver = (event) => {
+  event.preventDefault();
+}
+
+const onDrop = (event) => {
+  event.preventDefault();
+  const draggedCardId = event.dataTransfer.getData('id');
+  const draggedCard = document.getElementById(draggedCardId);
+  event.target.appendChild(draggedCard);
+  console.log('dropped the element');
+}
+
+rows.forEach((row, index) => {
+  const label = row.querySelector('.label');
+  label.style.backgroundColor = colors[index];
+  row.ondragover = onDragOver;
+  row.ondrop = onDrop;
+})
